08a582c64d13c36c37cf9ee0cb97d92e7a0c03ef
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-10-19 18:40:44 +0600

    Initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,90 @@
+import 'SearchPage.dart';
+import 'CartPage.dart';
+import 'MainPage.dart';
+import 'ProfilePage.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _deepBlue = Colors.orange[600];
+  final _backBottNavBar = const Color(0xFF2C2C33);
+  final backScreenColor = Colors.white;
+
+  int index = 0;
+
+  final screenList=[
+    const MainPage(),
+    const SearchPage(),
+    const CartPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:screenList[index],
+      backgroundColor: backScreenColor,
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor:_deepBlue,
+          backgroundColor: _backBottNavBar,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        child: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: (index) => setState(() {
+            this.index = index;
+          }),
+          destinations: [
+            NavigationDestination(
+              icon: Icon(
+                FontAwesomeIcons.bellConcierge,
+                color: backScreenColor,
+                size: 30,
+              ),
+              label: 'Restaraunts',
+            ),
+            NavigationDestination(
+              icon: Icon(
+                FontAwesomeIcons.search,
+                color: backScreenColor,
+                size: 30,
+              ),
+              label: 'Search',
+            ),
+            NavigationDestination(
+              icon: Icon(
+                FontAwesomeIcons.shoppingBag,
+                color: backScreenColor,
+                size: 30,
+              ),
+              label: 'Bag',
+            ),
+            NavigationDestination(
+              icon: Icon(
+                FontAwesomeIcons.userCircle,
+                color: backScreenColor,
+                size: 30,
+              ),
+              label: 'Account',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
