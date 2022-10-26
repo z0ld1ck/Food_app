bd3b4d185317f2d93de50ff2644ba169db3d08cf
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-10-26 19:15:19 +0600

    Initial commit

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor:Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2C2C33),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                final docUser =
+                    FirebaseFirestore.instance
+                        .collection('users')
+                        .doc('myQqY0t5OqLdXSda9xF5');
+
+                docUser.update({'name': 'loh'});
+              },
+              child: const Text('Update'),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final docUser =
+                    FirebaseFirestore.instance
+                        .collection('users')
+                        .doc('myQqY0t5OqLdXSda9xF5');
+
+
+                docUser.delete();
+              },
+              child:const Text('Delete'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
