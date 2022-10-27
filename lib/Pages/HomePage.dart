f320d015e7f5c3bac603e48b50d4b3d722e0342a
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-10-27 16:32:41 +0600

    Initial commit

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -10,11 +10,140 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF2C2C33),
-          ),
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
 
-      );
+
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF2C2C33),
+      appBar: AppBar(
+        backgroundColor:const Color(0xFF2C2C33),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(50),
+            topLeft: Radius.circular(50),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 28, left: 28),
+              child: const Text(
+                'Hi,User',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Color(0xff363636),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5, left: 20),
+              child: const Text(
+                'Welcome back',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xff363636),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 25, left: 28, right: 28),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0, 10),
+                    blurRadius: 15,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              height: 60,
+              width: size.width,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: const TextField(
+                        maxLines: 1,
+                        autofocus: false,
+                        style: TextStyle(
+                          color: Color(0xff107163),
+                          fontSize: 20,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "Search",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xff2a4b5e),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.search_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: size.width,
+              margin: const EdgeInsets.only(top: 20, left: 20),
+              child: Stack(
+                fit: StackFit.loose,
+                children: [
+                  Container(
+                    child: const Text(
+                      'Category',
+                      style: TextStyle(
+                        color: Color(0xff363636),
+                        fontSize: 20,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 10, top: 1),
+                    child: const Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'See all',
+                        style: TextStyle(
+                          color: Color(0xff363636),
+                          fontSize: 19,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
