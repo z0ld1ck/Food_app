3e146552fa54966e65a57df10b0f795a8a94fedb
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-11-02 20:59:37 +0600

    test page for navigation

---
+++ lib/category_details/category_seeall_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeeAll extends StatelessWidget {
+  const SeeAll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor:Colors.greenAccent,
+      ),
+      backgroundColor:Colors.white,
+    );
+  }
+}
