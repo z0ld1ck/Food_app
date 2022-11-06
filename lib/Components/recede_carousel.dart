04adfe4c5a1046cc918fd64b246e9ab164bea70e
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-11-07 02:14:36 +0600

    Replaced container for GestureDetector

--- lib/Components/recede_carousel.dart
+++ lib/Components/recede_carousel.dart
@@ -1,24 +1,22 @@
-import 'package:flutter/cupertino(1).dart';
 import 'package:flutter/material.dart';
+import '../category_details/category_seeall_detail_page.dart';
 
+// ignore: non_constant_identifier_names
 Widget Recede(String image) {
   return Row(
     children: [
       Container(
-        margin:const EdgeInsets.only(left: 10,right:20),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(10),
-            topLeft: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
+        margin: const EdgeInsets.all(5),
+        child: GestureDetector(
+          onTap: (){const SeeAll();},
+          child: Image.asset(
+            image,
+            fit: BoxFit.fitHeight, // Fixes border issues
+            width: 365,
+            height: 190,
           ),
         ),
-        height: 160,
-        width:350,
-        child: Image.asset(image),
-      ),
+      )
     ],
   );
 }
